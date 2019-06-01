82bcc64f5758b2f2ee0bfd6ae7358dbb33e6549b
David Hong <davehong@adobe.com>
2019-06-01 06:19:09 -0700

    [20190601] Fixing transitions between next and prev

--- projects/engee-datepicker-tester/src/app/engee-transition.directive.ts
+++ projects/engee-datepicker-tester/src/app/engee-transition.directive.ts
@@ -17,24 +17,30 @@ export class EngeeTransitionDirective implements OnChanges {
 
   constructor(private renderer: Renderer2, private el: ElementRef) {}
 
+  addClasses(classNames) {
+    const classNamesArr = classNames.split(' ');
+    classNamesArr.forEach(className => {
+      this.renderer.addClass(this.el.nativeElement, className);
+    });
+    return this;
+  }
+
+  removeClasses(classNames) {
+    const classNamesArr = classNames.split(' ');
+    classNamesArr.forEach(className => {
+      this.renderer.removeClass(this.el.nativeElement, className);
+    });
+    return this;
+  }
+
   ngOnChanges() {
     console.log('>>>', this.appEngeeTransition);
+    this.removeClasses('transitionWrapper--next transitionWrapper--prev');
     if (this.appEngeeTransition !== '') {
-      this.renderer.removeClass(
-        this.el.nativeElement,
-        'transitionWrapper--foo'
-      );
-      this.renderer.removeClass(
-        this.el.nativeElement,
-        `transitionWrapper--${this.appEngeeTransition}`
-      );
-      console.log('HERE');
+      this.addClasses('transitionWrapper--foo');
       setTimeout(() => {
-        console.log('HERE2');
-        this.renderer.addClass(
-          this.el.nativeElement,
-          `transitionWrapper--${this.appEngeeTransition}`
-        );
+        this.removeClasses('transitionWrapper--foo');
+        this.addClasses(`transitionWrapper--${this.appEngeeTransition}`);
       }, 200);
     }
   }
